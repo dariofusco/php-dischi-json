3b38ed8e1b4cce694d6a0d3976c6ec06936fc154
dariofusco <fdario@hotmail.it>
2023-09-11 15:47:19 +0200

    inserisce richiesta axios

--- api/dischi.php
+++ api/dischi.php
@@ -3,7 +3,7 @@
 $dischiJson = file_get_contents(__DIR__ . '/../db/dischi.json');
 
 header('Content-Type: application/json');
-header("Access-Control-Allow-Headers: X-Requested-With");
 
 echo $dischiJson;
+
 ?>
--- js/main.js
+++ js/main.js
@@ -1,9 +1,19 @@
-const app = Vue.createApp ({
+const app = Vue.createApp({
     data() {
         return {
+            dischi: [],
+        };
+    },
+    methods: {
 
-        }
-    }
+    },
+    mounted() {
+        axios.get("api/dischi.php").then(function (response) {
+            this.dischi = response.data;
+            console.log(response.data);
+        });
+    },
 });
 
 app.mount("#app");
+
